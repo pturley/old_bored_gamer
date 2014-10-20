2a87a14ccacd1febdff441ea20b45b3040eb93ad
Patrick Turley [pturley] <patrick.turley@gmail.com>
2014-10-19 23:06:55 -0500

    Search by category name now and display them

--- app/controllers/board_games_controller.rb
+++ app/controllers/board_games_controller.rb
@@ -2,7 +2,7 @@ class BoardGamesController < ApplicationController
 
   def index
     @q = BoardGame.search(params[:q])
-    @board_games = @q.result(distinct: true).paginate(:page => params[:page])
+    @board_games = @q.result(distinct: true).includes(:categories).paginate(:page => params[:page])
   end
 
 end
